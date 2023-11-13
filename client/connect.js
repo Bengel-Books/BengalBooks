92f9804a74c13067bf56acc4161df35e43a93b31
Kachi <99280287+kachi-1@users.noreply.github.com>
2023-11-13 14:32:34 -0600

    prototype book page made
    Book overview page now functioning, yet to implement buttons for reviewing and ordering.

--- client/connect.js
+++ client/connect.js
@@ -192,3 +192,230 @@
   
 //     });
 //   }
+
+
+//--------------------------------------------------------------------//
+//--------------------------------------------------------------------//
+//--------------------------------------------------------------------//
+// function CreateListings(){ //loop through each listing and display it
+//     //GET LISTINGS//
+//     //var token = document.cookie;
+//     //console.log('usertoken: ' + token);
+//     $(function () {
+//         $(function () {
+//             // let userToken = token;
+
+//             // let request = {
+//             //     "url": 'http://127.0.0.1:8000/listings',
+//             //     "method": "GET",
+//             //     "headers": { 'x-columbus-user-token': userToken},
+//             // };
+
+//             $.ajax(request).done(function (successResponse) {
+//                 console.log('Got listings' + JSON.stringify(successResponse));
+//                 for (i=0; i<successResponse.length; i++) {
+//                     let allListings = JSON.parse(JSON.stringify(successResponse))[i];
+//                     console.log(allListings);
+//                     listings.push(new Listing(allListings.record.title, allListings.record.images, allListings.record.description, allListings.record.contact, allListings.record.price[0], allListings.record.tags));
+//                     const currentDiv = document.getElementById("listings");
+//                     const child = document.createElement("div");
+//                     child.innerHTML = `<div class="listing"> 
+//                     <h3 id="lName">${allListings.record.title}</h3>
+//                     <img src=${allListings.record.images} id="lImage">
+//                     <p id="lDescription">
+//                         ${allListings.record.description}
+//                     </p>
+//                     <p id="lContact>
+//                         ${allListings.record.contact}
+//                     </p>
+//                     <p id="lPrice>$${allListings.record.price[0] + allListings.record.price[1] / 100}</p>
+                    
+//                     </div>`;
+                    
+//                     currentDiv.appendChild(child);
+//                     let btn = document.createElement("button");
+//                     btn.innerHTML = "Add to Wishlist";
+//                     btn.onclick = function () {makeWishlist(allListings.record.owner, allListings.id);AddToWishlist(new Listing(allListings.record.title, allListings.record.images, allListings.record.description, allListings.record.contact, allListings.record.price[0], allListings.record.tags));};
+
+//                     currentDiv.appendChild(btn);
+//                 }
+//                 console.log(listings);
+
+//             }).fail(function (failureResponse) {
+//                 console.log('Did not get listings');
+//             });
+//         })
+//     })
+    
+// }
+// function SortByName(){
+//     listings.sort((a, b) => (a.name > b.name) ? 1 : -1);
+//     ClearListings();
+//     ShowListings(listings);
+// }
+// function SortByPrice(){
+//     listings.sort((a, b) => (a.price > b.price) ? 1 : -1);
+//     ClearListings();
+//     ShowListings(listings);
+// }
+
+// function SearchByTag(){
+//     tagName = document.getElementById("tagName").value;
+//     foundListings = [];
+//     listings.forEach(listing => {
+//         tags = listing.tags;
+//         tags.forEach(tag =>{
+//             if (tag == tagName){
+//                 foundListings.push(listing);
+//             }
+//         })
+//     });
+//     ClearListings();
+//     ShowListings(foundListings);
+// }
+// function SortByRecent(){
+//     ClearListings();
+//     ShowListings(tempListings);
+// }
+
+// function PostWishlistItem(name, price, description) //FIXXXXXXXXXXXXXXXXXXXXXXXXXXXXX--------------------------
+// {
+//     let listingOwner = userEmail;
+//         let listingName = name;
+//         let listingPrice = [parseInt(price),parseInt((Number(price) - parseInt(price)) * 100)];
+//         let listingDes = description;
+//         let listingImg = [[0],[0]];
+//         var listing = {
+//             "id": 1,
+//             "owner": listingOwner,
+//             "title": listingName,
+//             "description": listingDes,
+//             "images": listingImg,
+//         };
+//         console.log(listing);
+
+//         $(function () {
+//             $(function () {
+//                 let userToken = document.cookie;
+    
+//                 let request = {
+//                     "url": "http://127.0.0.1:8000/current_user/wishlist",
+//                     "headers": { 'x-columbus-user-token': userToken},
+//                     "method": "POST",
+//                     "contentType": "application/json",
+//                     "data": JSON.stringify(listing),
+//                 };
+    
+//                 $.ajax(request).done(function (successResponse) {
+//                     console.log("You did it");
+//                     // Save the token and use it for future requests!
+//                 }).fail(function (failureResponse) {
+//                     console.log("Noooo");
+//                 });
+//             })
+//         })
+// }
+
+// function AddToWishlist(listing){
+//     wishlist.push(listing);
+//     PostWishlistItem(listing.name, listing.price, listing.description); 
+    
+//     UpdateWishlist();
+// }
+
+// function UpdateWishlist(){
+//     const currentDiv = document.getElementById("wishlist");
+//     ClearWishlist();
+//     let num=0;
+//     wishlist.forEach(listing => {
+//         const child = document.createElement("div");
+//         child.innerHTML = `<p>${listing.name}</p> 
+//         `;
+//         const child2 = document.createElement("button");
+//         child2.innerHTML = "remove";
+//         child2.onclick = function () {RemoveFromWishlist(num);};
+//         num = num + 1;
+//         child.appendChild(child2);
+//         currentDiv.appendChild(child);
+//     })
+//     //PostWishlistInfo(wishlist)
+    
+// }
+
+// function PostWishlistInfo(wishlist)
+// { //send the wishlist info to the database
+
+// }
+// function GetWishlistInfo()
+// {//GETs the wishlist info using the user token??
+
+// }
+
+// function RemoveFromWishlist(number){
+//     wishlist.splice(number - 1, 1);
+//     console.log("item "+ number + " removed");
+//     UpdateWishlist();
+// }
+
+// function ClearWishlist(){
+//     document.getElementById("wishlist").innerHTML = `<h3>Wish List</h3>`;
+// }
+
+
+// //CREATE WISHLIST//
+//   function makeWishlist(mail, iD) {
+//   $(function(){ 
+//         $(function () {
+//             $(function () {
+//                 let aListing = {
+//                     "user_email": mail,
+//                     "listing_id": iD
+//                 }
+//                 let listing = JSON.stringify(aListing);
+//                 console.log(listing);
+//                 let userToken = document.cookie;
+    
+//                 let request = {
+//                     "url": "http://127.0.0.1:8000/current_user/wishlist",
+//                     "headers": { 'x-columbus-user-token': userToken},
+//                     "method": "POST",
+//                     "contentType": "application/json",
+//                     "data": listing
+//                 };
+    
+//                 $.ajax(request).done(function (successResponse) {
+//                     console.log("You made a wishlist");
+//                     // Save the token and use it for future requests!
+//                 }).fail(function (failureResponse) {
+//                     console.log("No wishlist");
+//                 });
+//             })
+//         })
+
+//   });
+// }
+
+// $('#wish').click(function(){ 
+//           $(function () {
+//               $(function () {
+//                   let userToken = document.cookie;
+      
+//                   let request = {
+//                       "url": "http://127.0.0.1:8000/current_user/wishlist",
+//                       "headers": { 'x-columbus-user-token': userToken},
+//                       "method": "GET",
+//                   };
+      
+//                   $.ajax(request).done(function (successResponse) {
+//                       console.log("You got your wishlist");
+//                       let list = JSON.parse(JSON.stringify(successResponse))[0];
+//                       alert('Wishlist: ' + list.record.user_email + " id:" + list.id);
+                    
+//                       // Save the token and use it for future requests!
+//                   }).fail(function (failureResponse) {
+//                       console.log("You did not get your wishlist");
+//                   });
+//               })
+//           })
+  
+//     });
--- client/homepage.js
+++ client/homepage.js
@@ -1,10 +1,13 @@
 class Listing //
 {
-    constructor(name, images, description)
+    constructor(name, images, description, stock=5, status='available', reviews=['nice','good'])
     {
         this.name = name
         this.images = images;
         this.description = description;
+        this.stock = stock;
+        this.status = status;
+        this.reviews = reviews;
     }
 }
 let tempBooks = ["books/Ai_pearson.jpg","books/classix.jpeg"];
@@ -18,7 +21,11 @@ let tempDesc = [
 ]
 let listings = [];
 let tempListings = listings; //temporary listings for use by sort by recency button
-let wishlist = [];
+let wishlist = [listings[0]];
+let book = listings[0];
+var currindex = 0;
+
+const homeUrl = window.location.href;
 
 function makeListings() {
     for (i=0; i<tempBooks.length; i++) {
@@ -31,108 +38,101 @@ function makeListings() {
         <p id="lDescription">
             ${tempDesc[i]}
         </p>
+        <button type="button" id="button${i}" onclick="bookPage(${i})">Overview!</button>
+
         </div>`;
         
         currentDiv.appendChild(child);
-        let btn = document.createElement("button");
-        btn.innerHTML = "Overview";
-        btn.onclick = function () {window.open("profile.html","_self")};
+        // let btn = document.createElement("button");
+        // btn.innerHTML = "Overview";
+        // btn.onclick = function () {window.open("book.html","_self");};
     
-        currentDiv.appendChild(btn);
+        // currentDiv.appendChild(btn);
     }
     console.log(listings);
 }
+if (homeUrl.includes("homePage.html")) {
+    console.log(homeUrl);
+    var proButton = document.getElementById('profileButton');
+    proButton.addEventListener('click', function() {window.open("profile.html","_self")}); 
 
-makeListings()
+    makeListings()
+}
 
-
-
-// function CreateListings(){ //loop through each listing and display it
-//     //GET LISTINGS//
-//     //var token = document.cookie;
-//     //console.log('usertoken: ' + token);
-//     $(function () {
-//         $(function () {
-//             // let userToken = token;
-
-//             // let request = {
-//             //     "url": 'http://127.0.0.1:8000/listings',
-//             //     "method": "GET",
-//             //     "headers": { 'x-columbus-user-token': userToken},
-//             // };
-
-//             $.ajax(request).done(function (successResponse) {
-//                 console.log('Got listings' + JSON.stringify(successResponse));
-//                 for (i=0; i<successResponse.length; i++) {
-//                     let allListings = JSON.parse(JSON.stringify(successResponse))[i];
-//                     console.log(allListings);
-//                     listings.push(new Listing(allListings.record.title, allListings.record.images, allListings.record.description, allListings.record.contact, allListings.record.price[0], allListings.record.tags));
-//                     const currentDiv = document.getElementById("listings");
-//                     const child = document.createElement("div");
-//                     child.innerHTML = `<div class="listing"> 
-//                     <h3 id="lName">${allListings.record.title}</h3>
-//                     <img src=${allListings.record.images} id="lImage">
-//                     <p id="lDescription">
-//                         ${allListings.record.description}
-//                     </p>
-//                     <p id="lContact>
-//                         ${allListings.record.contact}
-//                     </p>
-//                     <p id="lPrice>$${allListings.record.price[0] + allListings.record.price[1] / 100}</p>
-                    
-//                     </div>`;
-                    
-//                     currentDiv.appendChild(child);
-//                     let btn = document.createElement("button");
-//                     btn.innerHTML = "Add to Wishlist";
-//                     btn.onclick = function () {makeWishlist(allListings.record.owner, allListings.id);AddToWishlist(new Listing(allListings.record.title, allListings.record.images, allListings.record.description, allListings.record.contact, allListings.record.price[0], allListings.record.tags));};
-
-//                     currentDiv.appendChild(btn);
-//                 }
-//                 console.log(listings);
-
-//             }).fail(function (failureResponse) {
-//                 console.log('Did not get listings');
-//             });
-//         })
-//     })
+function bookPage(index) {
+    sessionStorage.setItem("index", index);
+    sessionStorage.setItem("listings", JSON.stringify(listings));
+    window.open("book.html","_self");
     
-// }
-// function SortByName(){
-//     listings.sort((a, b) => (a.name > b.name) ? 1 : -1);
-//     ClearListings();
-//     ShowListings(listings);
-// }
-// function SortByPrice(){
-//     listings.sort((a, b) => (a.price > b.price) ? 1 : -1);
-//     ClearListings();
-//     ShowListings(listings);
-// }
+    // overview(currbook);
+    // console.log(currbook);
+    // console.log(index);
+}
 
-// function SearchByTag(){
-//     tagName = document.getElementById("tagName").value;
-//     foundListings = [];
-//     listings.forEach(listing => {
-//         tags = listing.tags;
-//         tags.forEach(tag =>{
-//             if (tag == tagName){
-//                 foundListings.push(listing);
-//             }
-//         })
-//     });
-//     ClearListings();
-//     ShowListings(foundListings);
-// }
-// function SortByRecent(){
-//     ClearListings();
-//     ShowListings(tempListings);
-// }
+if (homeUrl.includes("book.html")) {
+    console.log(homeUrl);
+    let books = JSON.parse(sessionStorage.getItem("listings"));
+    console.log(books);
+    let book = books[sessionStorage.getItem("index")];
+    console.log(book);
+    var homeButton = document.getElementById('homeButton');
+    homeButton.addEventListener('click', function() {window.open("homePage.html","_self")}); 
+    
+    overview(book);
+
+}
+
+function overview(book) {
+    console.log(book.name);
+    let reviews = "<ul>";
+    let i = 0;
+    while (i < book.reviews.length) {
+        reviews = reviews.concat("<li>" + book.reviews[i] + "</li>");
+        i++;
+    }
+    reviews = reviews.concat("</ul>")
+    console.log(reviews);
+    const currentDiv = document.getElementById("overview");
+    const child = document.createElement("div");
+    child.innerHTML = `<div class="overview"> 
+    <h3>${book.name}</h3>
+    <img src=${book.images} height=100 width=100>
+    <p>
+        ${book.description}
+    </p>
+    <div class="reviews1">
+    <p>    
+        Reviews:${(reviews)}
+    </p>
+    <button type="button" id="reviewButton" class="reviewButton">Add Review</button>
+    </div>
+    </div>`;
+    
+    currentDiv.appendChild(child);
+    // let btn = document.createElement("button");
+    // btn.innerHTML = "Add to Wishlist";
+    // btn.onclick = function () {AddToWishlist(listing);};
+    // currentDiv.appendChild(btn);
+
+    
+    console.log(book);
+}
+
+
+
 
 function Search(){ //for search bar
     thingToSearch = document.getElementById("searchInput").value;
     console.log(thingToSearch);
     foundListings = [];
-    listings.forEach(listing => {
+    let currListings = listings;
+    
+    if (homeUrl.includes("homePage.html")) {
+        currListings = listings;
+    }else if (homeUrl.includes("profile.html")) {
+        currListings = wishlist;
+    }
+    currListings.forEach(listing => {
         n = listing.name;
         
         b = n.toLowerCase().includes(thingToSearch.toLowerCase());
@@ -142,6 +142,7 @@ function Search(){ //for search bar
     });
     ClearListings();
     ShowListings(foundListings);
+
 }
 
 $("#searchBar").on("change keyup paste", function(){
@@ -175,150 +176,7 @@ function ClearListings(){
     currentDiv.innerHTML = "";
 }
 
-var proButton = document.getElementById('profileButton');
-proButton.addEventListener('click', function() {window.open("profile.html","_self")}); 
-
-var homeButton = document.getElementById('homeButton');
-homeButton.addEventListener('click', function() {window.open("homePage.html","_self")}); 
-
-// function PostWishlistItem(name, price, description) //FIXXXXXXXXXXXXXXXXXXXXXXXXXXXXX--------------------------
-// {
-//     let listingOwner = userEmail;
-//         let listingName = name;
-//         let listingPrice = [parseInt(price),parseInt((Number(price) - parseInt(price)) * 100)];
-//         let listingDes = description;
-//         let listingImg = [[0],[0]];
-//         var listing = {
-//             "id": 1,
-//             "owner": listingOwner,
-//             "title": listingName,
-//             "description": listingDes,
-//             "images": listingImg,
-//         };
-//         console.log(listing);
-
-//         $(function () {
-//             $(function () {
-//                 let userToken = document.cookie;
-    
-//                 let request = {
-//                     "url": "http://127.0.0.1:8000/current_user/wishlist",
-//                     "headers": { 'x-columbus-user-token': userToken},
-//                     "method": "POST",
-//                     "contentType": "application/json",
-//                     "data": JSON.stringify(listing),
-//                 };
-    
-//                 $.ajax(request).done(function (successResponse) {
-//                     console.log("You did it");
-//                     // Save the token and use it for future requests!
-//                 }).fail(function (failureResponse) {
-//                     console.log("Noooo");
-//                 });
-//             })
-//         })
-// }
-
-// function AddToWishlist(listing){
-//     wishlist.push(listing);
-//     PostWishlistItem(listing.name, listing.price, listing.description); 
-    
-//     UpdateWishlist();
-// }
-
-// function UpdateWishlist(){
-//     const currentDiv = document.getElementById("wishlist");
-//     ClearWishlist();
-//     let num=0;
-//     wishlist.forEach(listing => {
-//         const child = document.createElement("div");
-//         child.innerHTML = `<p>${listing.name}</p> 
-//         `;
-//         const child2 = document.createElement("button");
-//         child2.innerHTML = "remove";
-//         child2.onclick = function () {RemoveFromWishlist(num);};
-//         num = num + 1;
-//         child.appendChild(child2);
-//         currentDiv.appendChild(child);
-//     })
-//     //PostWishlistInfo(wishlist)
-    
-// }
-
-// function PostWishlistInfo(wishlist)
-// { //send the wishlist info to the database
-
-// }
-// function GetWishlistInfo()
-// {//GETs the wishlist info using the user token??
-
-// }
-
-// function RemoveFromWishlist(number){
-//     wishlist.splice(number - 1, 1);
-//     console.log("item "+ number + " removed");
-//     UpdateWishlist();
-// }
-
-// function ClearWishlist(){
-//     document.getElementById("wishlist").innerHTML = `<h3>Wish List</h3>`;
-// }
 
 
-// //CREATE WISHLIST//
-//   function makeWishlist(mail, iD) {
-//   $(function(){ 
-//         $(function () {
-//             $(function () {
-//                 let aListing = {
-//                     "user_email": mail,
-//                     "listing_id": iD
-//                 }
-//                 let listing = JSON.stringify(aListing);
-//                 console.log(listing);
-//                 let userToken = document.cookie;
-    
-//                 let request = {
-//                     "url": "http://127.0.0.1:8000/current_user/wishlist",
-//                     "headers": { 'x-columbus-user-token': userToken},
-//                     "method": "POST",
-//                     "contentType": "application/json",
-//                     "data": listing
-//                 };
-    
-//                 $.ajax(request).done(function (successResponse) {
-//                     console.log("You made a wishlist");
-//                     // Save the token and use it for future requests!
-//                 }).fail(function (failureResponse) {
-//                     console.log("No wishlist");
-//                 });
-//             })
-//         })
 
-//   });
-// }
 
-// $('#wish').click(function(){ 
-//           $(function () {
-//               $(function () {
-//                   let userToken = document.cookie;
-      
-//                   let request = {
-//                       "url": "http://127.0.0.1:8000/current_user/wishlist",
-//                       "headers": { 'x-columbus-user-token': userToken},
-//                       "method": "GET",
-//                   };
-      
-//                   $.ajax(request).done(function (successResponse) {
-//                       console.log("You got your wishlist");
-//                       let list = JSON.parse(JSON.stringify(successResponse))[0];
-//                       alert('Wishlist: ' + list.record.user_email + " id:" + list.id);
-                    
-//                       // Save the token and use it for future requests!
-//                   }).fail(function (failureResponse) {
-//                       console.log("You did not get your wishlist");
-//                   });
-//               })
-//           })
-  
-//     });
