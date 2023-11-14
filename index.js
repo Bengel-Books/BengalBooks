fcdfe8cac43c7bc85abf8ab8e6f43cba8d2046da
SethDMill <124477422+SethDMill@users.noreply.github.com>
2023-11-13 19:15:07 -0600

    Added login button functionality

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 var homeButton = document.getElementById('homeButton');
 homeButton.addEventListener('click', function() {window.open("client/homePage.html","_self")}); 
+
+var loginButton = document.getElementById('loginButton');
+loginButton.addEventListener('click', function() {window.open("client/login.html","_self")}); 
